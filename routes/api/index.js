5d1f7acb7f169874579f4154e6079dc8d4dabc05
Michael Iringan <miringan@uw.edu>
2022-03-25 20:43:24 -0700

    added code to userRoutes. updated code in index.js.

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const friendRoutes = require('./userRoutes');
 const thoughtRoutes = require('./thoughtRoutes');
 
-router.use('/users', userRoutes, friendRoutes);
+router.use('/users', userRoutes);
 router.use('/thoughts', thoughtRoutes);
 
 module.exports = router;
